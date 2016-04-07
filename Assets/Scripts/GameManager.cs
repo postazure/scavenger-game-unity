3ca77716468ea7a55bc3d3f59581aa457be423a8
Max Gordon <gordonmaxc@gmail.com>
2016-04-06 21:10:26 -0700

    game loads on startup

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,12 +2,20 @@
 using System.Collections;
 
 public class GameManager : MonoBehaviour {
+	public static GameManager instance = null;
 
 	public BoardManager boardScript;
 
 	private int level = 3;
 
 	void Awake(){
+		if (instance == null) {
+			instance = this;
+		} else if (instance != this) {
+			Destroy (gameObject);
+		}
+
+		DontDestroyOnLoad (gameObject);
 		boardScript = GetComponent<BoardManager> ();
 		InitGame ();
 	}
